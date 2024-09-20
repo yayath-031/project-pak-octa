d3ca19a3ee4ed131da7db0844041decd6c416d79
yayath-031 <yaynature01@gmail.com>
2024-09-20 16:29:57 +0800

    kumpul dataset musik

---
+++ Dataset/H071241003.py
@@ -0,0 +1,14 @@
+musik = [
+    {
+        "judul": "test",
+        "artis": ['a', 'b'],
+        "bahasa": "a",
+        "genre": "a"
+    },
+    {
+        "judul": "test",
+        "artis": ['a', 'b'],
+        "bahasa": "a",
+        "genre": "a"
+    }
+]
